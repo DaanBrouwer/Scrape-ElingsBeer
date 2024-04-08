98e2b232f92f038d3c14c41849c46d3650534ea9
Daan Brouwer <dhwbrouwer@outlook.com>
2024-04-08 19:46:43 +0200

    Remove unnecesary usings

--- SeleniumScrapeWebsite/Appsettings.cs
+++ SeleniumScrapeWebsite/Appsettings.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReadHTML
+﻿namespace ReadHTML
 {
     public record Appsettings
     {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required string OutputPath { get; set; }
     }
 }
--- SeleniumScrapeWebsite/Program.cs
+++ SeleniumScrapeWebsite/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenQA.Selenium;
 using ReadHTML;
@@ -11,18 +9,6 @@ using ReadHTML.Interfaces;
 // dubbel check brouwerij untappd of deze echt goed is vergelijk met bekende brouwerij van elings
 // if rating > 3.5 ofzo check checkins
 // fix error edgedriver
-//var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
-//{
-//    var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build(); ;
-//    services.Configure<Appsettings>(configuration.GetSection("AppSettings"));
-//    services.AddSingleton<IWebDriver, WebDriver>();
-//    services.AddSingleton<IWebElement, WebElement>();
-//    services.AddSingleton<IProcessElings, ProcessElings>();
-//    services.AddSingleton<IProcessUntappd, ProcessUntappd>();
-//    services.AddSingleton<IExportCSV, ExportCSV>();
-//    services.AddSingleton<IProcessBeers, ProcessBeers>();
-//}).Build();
-
 var builder = Host.CreateApplicationBuilder();
 
 builder.Services
@@ -34,6 +20,8 @@ builder.Services
     .AddSingleton<IExportCSV, ExportCSV>()
     .AddSingleton<IProcessBeers, ProcessBeers>();
 
+var host = builder.Build();
+var app = host.Services.GetRequiredService<IProcessBeers>();
 app.ProcessAllBeer();
 
 
